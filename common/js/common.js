813eb849660ab03ae686fe2034480acba9d8f4e3
i yashiro <i.sayaka.yashiro@gmail.com>
2025-03-18 18:28:13 +0900

    opening anime, page top icon

--- common/js/common.js
+++ common/js/common.js
@@ -1,6 +1,6 @@
 $(function(){
 
-	$("#op").delay(1800).fadeOut(800);
+	$("#op").delay(6000).fadeOut(100);
 
 	$("#menuButton, #globalNav a").click( function(){
 		$("#globalNav").fadeToggle(300);
@@ -25,53 +25,35 @@ $(function(){
 				$(this).removeClass("on");
 			}
 		});
-	});
 
-  $(".propertySlider.left").slick({
-    autoplay:true,//自動再生する
-    autoplaySpeed: 0,//自動再生するスピード 4秒
-    speed: 10000,
-		cssEase: "linear",
-    fade: false,//フェードする
-    infinite:true,//無限にスライドする
-    slidesToShow: 2, // 表示するスライドの数
-    arrows: false, // 矢印の表示
-    dots:false,//ドット部分を表示する
-		centerMode: true,//スライドを中心に表示して、部分的に前後のスライドが見えるように設定
-    centerPadding: "17.4%",//centerMode時に前後のスライドが見える幅（px or ％）（初期値：50px）
-    pauseOnFocus: false,//フォーカスで一時停止
-    pauseOnHover: false,//マウスホバーで一時停止
-    pauseOnDotsHover: false,//ドットナビをマウスホバーで一時停止
-    responsive: [{
-      breakpoint: 821,
-      settings: {
-        slidesToShow: 1,
-      }
-    }]
-  });
-  $(".propertySlider.right").slick({
-    autoplay:true,//自動再生する
-    autoplaySpeed: 0,//自動再生するスピード 4秒
-    speed: 10000,
-		cssEase: "linear",
-    fade: false,//フェードする
-    infinite:true,//無限にスライドする
-    slidesToShow: 2, // 表示するスライドの数
-    arrows: false, // 矢印の表示
-    dots:false,//ドット部分を表示する
-		centerMode: true,//スライドを中心に表示して、部分的に前後のスライドが見えるように設定
-    centerPadding: "17.4%",//centerMode時に前後のスライドが見える幅（px or ％）（初期値：50px）
-    pauseOnFocus: false,//フォーカスで一時停止
-    pauseOnHover: false,//マウスホバーで一時停止
-    pauseOnDotsHover: false,//ドットナビをマウスホバーで一時停止
-		rtl: true,
-    responsive: [{
-      breakpoint: 821,
-      settings: {
-        slidesToShow: 1,
-				centerPadding: "24.8%",
-      }
-    }]
-  });
+		$(".pageTop").each(function(){
+			var imgPos = $(this).offset().top;
+			var scroll = $(window).scrollTop();
+
+			var windowHeight = $(window).height();
+			if (window.matchMedia("(max-width: 880px)").matches) {
+				var scrollHeight = windowHeight/5;
+			} else {
+				var scrollHeight = windowHeight/3;
+			}
+
+			if (scroll > scrollHeight) {
+				$(this).fadeIn(600);
+			} else {
+				$(this).fadeOut(600);
+			}
+
+			var mainHeight = $("main").innerHeight();
+			var storyHeight = $("#story").innerHeight();
+			var messageHeight = $("#message").innerHeight();
+
+			if (imgPos >  mainHeight - storyHeight - messageHeight) {
+				$(this).addClass("on");
+			} else {
+				$(this).removeClass("on");
+			}
+
+		});
+	});
 
 });
