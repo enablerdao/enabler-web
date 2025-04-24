df74cfc7728572506070168c5d8881fd6b1c1b9d
Yuki Hamada <mail@yukihamada.jp>
2025-04-25 00:13:49 +0900

    Fix build error: use ESM vite.config.mjs and adjust build script

---
+++ vite.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+export default defineConfig({
+  base: './',
+  build: {
+    rollupOptions: {
+      input: resolve('.', 'index.html')
+    }
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [{ src: 'common', dest: '' }]
+    })
+  ]
+}); 
